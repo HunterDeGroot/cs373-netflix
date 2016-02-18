932ef4d48ef5b9ba416319634fba76cd16b2bc5d
Hunter DeGroot <hunterdegroot@gmail.com>
2016-02-17 20:51:36 -0600

    changing caches

--- Netflix.py
+++ Netflix.py
@@ -8,17 +8,18 @@
 
 import os
 import requests
+import pickle
 
 from urllib.request import urlopen
 
 
 def netflix_solve () :
-	if os.path.isfile('http://www.cs.utexas.edu/users/downing/public_html/netflix-caches/ad35988-movie_stddev_average.pickle') :
-		f = open('http://www.cs.utexas.edu/users/downing/public_html/netflix-caches/ad35988-movie_stddev_average.pickle','rb')
+	if os.path.isfile('http://www.cs.utexas.edu/users/downing/public_html/netflix-caches/kh549-movie_average.pickle') :
+		f = open('http://www.cs.utexas.edu/users/downing/public_html/netflix-caches/kh549-movie_average.pickle','rb')
 		user_cache = pickle.load(f)
 	else:
 		# Read cache from HTTP
-		bytes = urlopen('http://www.cs.utexas.edu/users/downing/netflix-caches/ad35988-movie_stddev_average.pickle').read()
+		bytes = urlopen('http://www.cs.utexas.edu/users/downing/netflix-caches/kh549-movie_average.pickle').read()
 		user_cache = pickle.loads(bytes)
 		
 		# use user_cache here
